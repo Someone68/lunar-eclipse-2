67ead7d84d345a8757730aeb86342c62e46ae302
Someone68 <felix002m+1@gmail.com>
2024-10-10 15:07:09 -0700

    cloaking fix 1

--- script.js
+++ script.js
@@ -100,9 +100,9 @@ function customdis() {
 }
 
 function cloak() {
+  location.replace(localStorage.panicurl);
   tab = window.open();
   tab.window.document.body.innerHTML = `<!DOCTYPE html><html><head><style>*{padding:0;margin:0;}html,body{width:100%;height:100vh}</style></head><body><iframe src="https://${window.location.host}" style="border:0px #ffffff none;" name="lunar" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="100%" width="100%" allowfullscreen></iframe></body></html>`;
-  location.replace(localStorage.panicurl);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
